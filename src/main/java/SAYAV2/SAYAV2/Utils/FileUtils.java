140ea71e18961b250445cac18ab9cb035f13df31
isaiasarza <arza.isaias@gmail.com>
2017-06-21 17:07:24 -0300

    Finishhhh

--- src/main/java/SAYAV2/SAYAV2/Utils/FileUtils.java
+++ src/main/java/SAYAV2/SAYAV2/Utils/FileUtils.java
@@ -1,15 +1,52 @@
-package SAYAV2.SAYAV2.Utils;
+package SAYAV2.Utils;
 
 public class FileUtils {
+	public static final String CONFIGURATOR_FILE = "configurator";
+	public static final String USUARIO_FILE = "SAYAV";
+	public static final String NOTIFICACIONES_FILE = "notificaciones";
+	public static final String VOTACIONES_FILE = "votaciones";
+	public static final String VOTACIONES_PENDIENTES_FILE = "votaciones_pendientes";
+	public static final String MENSAJES_FILE = "mensajes";
+	public static final String TIPOS_MENSAJES_FILE = "tipos_mensajes";
+	public static final String WORKSPACE_PATH = "";
 
-//	public static File searchFile(String path){
-//		File f = new File(path);
-//	
-//		File aux = null;
-//		File[] files = f.listFiles();
-//		for(File file: files)
-//			if(file.getPath().equals(path))
-//				aux = file;
-//		return aux;
-//	}
+	public static String getWORKSPACE_PATH() {
+		return WORKSPACE_PATH;
+	}
+
+	public static String getConfiguratorFile() {
+		return CONFIGURATOR_FILE;
+	}
+
+	public static String getUsuarioFile() {
+
+		return USUARIO_FILE;
+
+	}
+
+	public static String getNotificacionesFile() {
+		return NOTIFICACIONES_FILE;
+
+	}
+
+	public static String getVotacionesFile() {
+		return VOTACIONES_FILE;
+	}
+
+	public static String getVotacionesPendientesFile() {
+		return VOTACIONES_PENDIENTES_FILE;
+	}
+
+	public static String getMensajesFile() {
+		return MENSAJES_FILE;
+	}
+
+	public static String getTiposMensajesFile() {
+		return TIPOS_MENSAJES_FILE;
+	}
+
+	public static String getWorkspacePath() {
+		return WORKSPACE_PATH;
+	}
+
 }
